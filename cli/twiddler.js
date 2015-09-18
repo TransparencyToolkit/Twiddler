d0ae540daa1d5803532fe62eb1251c4fa53f12b2
Brennan Novak <hi@bnvk.me>
2015-09-18 19:58:40 +0200

    added basic CLI launcher

--- cli/twiddler.js
+++ cli/twiddler.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
-
+'use strict'
 var pkg = require('package')('./')
 var path = require('path')
-var _ = require('underscore')
-var chalk = require('chalk')
 var program = require('commander')
-
-//var TextStats = require('text-stats')
 
 // Config
 var Config = require('../lib/config').Init()
@@ -17,9 +13,9 @@ var Twiddler = {}
 program
   .version(pkg.version)
   .command('new <dir>', 'Create a new project file')
-  .command('open <dir>', 'Open an existing project file')
-  .command('source <dir>', 'Add source to existing project')
-  .command('process <dir>', 'Process a project file')
+  .command('open <project>', 'Open an existing project file')
+  .command('source <project> [type]', 'Add source to existing project', /^(file|folder|string)$/i, 'file')
+  .command('process <project>', 'Process and output a project file')
   .parse(process.argv)
 
 module.exports = Twiddler
