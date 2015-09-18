ddf70d5b2cdba3e9c57f323e025edecd998dcdea
Brennan Novak <hi@bnvk.me>
2015-09-18 19:26:35 +0200

    added wrapper of libs app.js for ease

---
+++ app.js
@@ -0,0 +1,14 @@
+var App = {}
+
+App.Config      = require('./lib/config')
+App.Files       = require('./lib/files')
+App.Formatter   = require('./lib/formatter')
+App.Hasher      = require('./lib/hasher')
+App.Outputer    = require('./lib/outputer')
+App.Processor   = require('./lib/processor')
+App.Project     = require('./lib/project')
+App.Templates   = require('./lib/templates')
+App.Tools       = require('./lib/tools')
+App.Utils       = require('./lib/utils')
+
+module.exports = App
