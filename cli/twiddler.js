3d2476c400565b2851b65a3c88f912d1d66b69fa
Brennan Novak <hi@bnvk.me>
2015-09-22 04:22:50 +0200

    better CLI feedback on sources

--- cli/twiddler.js
+++ cli/twiddler.js
@@ -17,9 +17,16 @@ Cli.PrintProject = function(project) {
   console.log(chalk.green('  ' + project.title + ' (' + project.name + ')'))
   console.log(chalk.green('  Description: ' + project.description))
   console.log(chalk.green('  Sources: ' + project.sources.length))
+  var source_count = 0
   _.each(project.sources, function(source, count) {
+    if (source_count < 5) {
     console.log(chalk.green('    - ' + source.source))
+    source_count++
+    }
   })
+  if (project.sources.length > 5) {
+    console.log(chalk.green('    ...' + (project.sources.length - 5) + ' more sources...'))
+  }
   console.log(chalk.green('  Tools: ' + _.pluck(project.tools, 'tool').join(', ')))
   console.log(chalk.green('  Output: ' + project.output))
   console.log(chalk.green('  Formats: ' + project.formats.join(', ')))
@@ -33,6 +40,7 @@ program
   .command('open <project>', 'Open an existing project file')
   .command('source <project>', 'Add source to existing project')
   .command('process <project>', 'Process and output a project file')
+  //.command('projects <dir>', 'Show a list of all projects')
   .parse(process.argv)
 
 module.exports = Cli
